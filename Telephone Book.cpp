f1581874b5a0fc121cc380e05cb82b8d32c27259
Parth Khajgiwale <90266917+parthkhajgiwale@users.noreply.github.com>
2023-05-30 06:49:48 +0530

    Telephone Book.cpp

--- Telephone Book.cpp
+++ Telephone Book.cpp
@@ -1,96 +1,151 @@
-/*Consider telephone book database of N clients. Make use of a hash table implementation to quickly
-look up client‘s telephone number*/
+/*
+Consider telephone book database of N clients. 
+Make use of a hash table implementation to quickly look up client‘s telephone number.
+*/ 
+    #include<iostream>
+    #include<cstdlib>
+    #include<string>
+    #include<cstdio>
+    using namespace std;
+    const int TABLE_SIZE = 20;
 
-#include<iostream>
-#include<string.h>
-using namespace std;
-struct data
-{  char name[30];
-   long teleno;
-};
-class hash
-{  int n,sum,x,c,i,j;   char na[30]; long no;
-   data d[10];
-   public:
-   hash()
-   {  for(i=0;i<10;i++)
-      {  d[i].teleno=0;   }
-   }  
-   void insert();
-   void search();
-   void display();
-};
-void hash::insert()
-{
-  cout<<"\n enter no. of clients";
-  cin>>n;    //d[x].name
-  for(j=0;j<n;j++)
-  {   cout<<"\n enter name of client";
-      cin>>na;
-      cout<<"\n enter telephone no. of client";
-      cin>>no;
-      sum=0;
-      for(i=0;i<strlen(na);i++)
-      {   sum=sum+na[i]; 
-      }
-      x=(sum/strlen(na))%10;
-      cout<<x;
-      c=x;
-           while(1)
-           {
-          
-               if(d[x].teleno==0)
-               {   strcpy(d[x].name,na);
-                   d[x].teleno=no;
-                   break;
-               }
-                x=(x+1)%10;
-              if(c==x)
-              {  cout<<"\n hash table is full";
-                  break;
-              }   
-           }
+    class HashEntry
+    {
+        public:
+            int key;
+            int value;
+            HashEntry(int key, int value)//parameteized constructor
+            {
+                this->key = key;
+                this->value = value;
+            }
+    };//end of class
 
-   }
-}
-void hash::search()
-{  cout<<"\n enter name to be searched";
-   cin>>na;
-  
-    sum=0;
-      for(i=0;i<strlen(na);i++)
-      {   sum=sum+(int)na[i]; 
-      }
-      x=(sum/strlen(na))%10;
-      c=x;
-   while(1)
-           {
-          
-               if(!strcmp(d[x].name,na))
-               {   cout<<"\n data found : TELEPHONE NO:"<<d[x].teleno;
-                   break;
-               }
-                x=(x+1)%10;
-              if(c==x)
-              {  cout<<"\n data not found";
-                  break;
-              }   
+    class HashMap
+
+    {
+        private:
+            HashEntry **table;
+        public:
+            HashMap()
+     {
+                table = new HashEntry * [TABLE_SIZE];
+                for (int i = 0; i< TABLE_SIZE; i++)
+                {
+                    table[i] = NULL;
+                }
+            }
+            int HashFunc(int key)//division hash function
+            {
+                return key % TABLE_SIZE;
+            }
+     void Insert(int key, int value)//insert operation
+     {
+                int hash = HashFunc(key);
+                while (table[hash] != NULL && table[hash]->key != key)
+                {
+                    hash = HashFunc(hash + 1);
+                }
+                if (table[hash] != NULL)
+
+                    delete table[hash];
+                table[hash] = new HashEntry(key, value);
+     }
+            int Search(int key)//search operation
+     {
+         int  hash = HashFunc(key);
+         while (table[hash] != NULL && table[hash]->key != key)
+         {
+             hash = HashFunc(hash + 1);
+         }
+         if (table[hash] == NULL)
+             return -1;
+         else
+             return table[hash]->value;
+            }
+
+            void Remove(int key)
+     {
+         int hash = HashFunc(key);
+         while (table[hash] != NULL)
+         {
+             if (table[hash]->key == key)
+                 break;
+
+             hash = HashFunc(hash + 1);
+         }
+                if (table[hash] == NULL)
+         {
+                    cout<<"No Element found at key "<<key<<endl;
+                    return;
+                }
+                else
+                {
+                    delete table[hash];
+                }
+                cout<<"Element Deleted"<<endl;
+            }
+            ~HashMap()//destructor
+     {
+                for (int i = 0; i < TABLE_SIZE; i++)
+                {
+                    if (table[i] != NULL)
+                        delete table[i];
+                    delete[] table;
+                }
+            }
+    };
+
+    int main()//program execution starts from here
+    {
+        HashMap hash;
+        int key, value;
+        int choice;
+        while (1)
+        {
+            cout<<"\n----------------------"<<endl;
+            cout<<"Operations on Hash Table"<<endl;
+            cout<<"\n----------------------"<<endl;
+            cout<<"1.Insert element into the table"<<endl;
+            cout<<"2.Search element from the key"<<endl;
+            cout<<"3.Delete element at a key"<<endl;
+            cout<<"4.Exit"<<endl;
+            cout<<"Enter your choice: ";
+            cin>>choice;
+            switch(choice)
+            {
+            case 1:
+                cout<<"Enter element to be inserted: ";
+                cin>>value;
+                cout<<"Enter key at which element to be inserted: ";
+                cin>>key;
+                hash.Insert(key, value);
+                break;
+
+            case 2:
+                cout<<"Enter key of the element to be searched: ";
+                cin>>key;
+                if (hash.Search(key) == -1)
+                {
+             cout<<"No element found at key "<<key<<endl;
+             continue;
+         }
+         else
+         {
+             cout<<"Element at key "<<key<<" : ";
+             cout<<hash.Search(key)<<endl;
+         }
+                break;
+            case 3:
+                cout<<"Enter key of the element to be deleted: ";
+                cin>>key;
+                hash.Remove(key);
+                break;
+            case 4:
+                exit(1);
+            default:
+               cout<<"\nEnter correct option\n";
            }
-}          
-void hash::display()
-{
-        for(int i=0;i<10;i++){
-        cout<<endl<<d[i].name<<" "<<d[i].teleno;
         }
-
-
-}
-int main()
-{
-  hash h;
-  h.insert();
- // h.display();
-   h.search();
-//   h.display();
-  return 0;
-} 
+        return 0;
+    }
